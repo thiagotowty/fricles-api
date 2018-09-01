727f70d27bbaac42941b45b22ed720dc91393834
Thiago <thiago@towty.com.br>
2018-09-01 19:45:31 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'whebhook'], function ()
 {
-    Route::post('', 'FacebbokController@webhook');
+    Route::get('', 'FacebbokController@webhook');
 });
