a7c71ca8a8a3b0739de42dc80dcc29425bd84696
Thiago <thiago@towty.com.br>
2018-09-01 19:54:33 -0300

    teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -14,9 +14,9 @@ class FacebookController extends Controller
         $hub_verify_token  = $request->hub_verify_token;
 
         if ($verify_token == $hub_verify_token)
-            return response()->json($hub_challenge, 200);
+            return response($hub_challenge, 200);
 
-        return response()->json(null, 404);
+        return response(null, 403);
 
     }
 }
