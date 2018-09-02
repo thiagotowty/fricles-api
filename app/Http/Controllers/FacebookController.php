246c5d1f0c8b01c7bb21a57e058d9f33c70e0f6e
Thiago <thiago@towty.com.br>
2018-09-01 22:28:44 -0300

    teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -24,26 +24,28 @@ class FacebookController extends Controller
     {
         Log::info('entrou');
 
-        $sender_id = $request->sender->id;
-        $recipient_id = $request->recipient->id;
+        Log::info(dd($request));
 
-        Log::info('$sender_id ' . $sender_id);
-        Log::info('$recipient_id ' . $recipient_id);
+//        $sender_id = $request->sender->id;
+//        $recipient_id = $request->recipient->id;
 
-        $retorno = new class{};
-        $retorno->sender = new class {};
-        $retorno->sender->id = $sender_id;
-
-        $retorno->recipient = new class {};
-        $retorno->recipient->id = $recipient_id;
-
-        $retorno->message = new class{};
-        if ($request->message->text == 1) {
-            $retorno->message->text = "deu certo";
-        }
-
-        $retorno->message->text = "não deu certo";
-
-        return response()->json($retorno, 200);
+//        Log::info('$sender_id ' . $sender_id);
+//        Log::info('$recipient_id ' . $recipient_id);
+//
+//        $retorno = new class{};
+//        $retorno->sender = new class {};
+//        $retorno->sender->id = $sender_id;
+//
+//        $retorno->recipient = new class {};
+//        $retorno->recipient->id = $recipient_id;
+//
+//        $retorno->message = new class{};
+//        if ($request->message->text == 1) {
+//            $retorno->message->text = "deu certo";
+//        }
+//
+//        $retorno->message->text = "não deu certo";
+//
+//        return response()->json($retorno, 200);
     }
 }
