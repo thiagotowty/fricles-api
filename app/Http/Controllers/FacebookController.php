ff6e2bf9b455a9067585c898c4d7b902fb037874
Thiago <thiago@towty.com.br>
2018-09-02 10:13:56 -0300

    teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -26,31 +26,31 @@ class FacebookController extends Controller
         Log::info('post');
         Log::warning('REQUEST ' . $request);
 
-//        $object = new class{};
-//        $object->messaging_type = "RESPONSE";
-//        $recipient = new class{};
-//        $recipient->id = $request->entry[0]["messaging"][0]["sender"]["id"];
-//        $message = new class{};
-//        $message->text = "Teste legal";
-//
-//        $object->recipient = $recipient;
-//        $object->message = $message;
+        $object = new class{};
+        $object->messaging_type = "RESPONSE";
+        $recipient = new class{};
+        $recipient->id = $request->entry[0]["messaging"][0]["sender"]["id"];
+        $message = new class{};
+        $message->text = "Teste legal";
 
-//        $access_token = "EAAFdwO6fUOcBAJZCgCH2IHTuSezOyl2oK18Fyqs8LY5ZBEB8iZA7mtWBnl5xvSzeiWazemBXn1ZB4pqNZAyUVyHuUPu1oet4pk5ihehZBAZAZAAIVZAQFe4r2jlLngIzo062bCZAc2pMLvwmCc2a0hZCV24uwuEJbr2SZAIZC1CRXG18kelDmkpEN4k8H";
-//        $curl = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=".$access_token);
-//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($object));
-//        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-//                'Content-Type: application/json'
-//        ));
-////        curl_exec($curl);
-//        $retorno = curl_exec($curl);
-//        $info = curl_getinfo($curl);
-//        curl_close($curl);
+        $object->recipient = $recipient;
+        $object->message = $message;
 
-//        Log::info("======================================================================");
-//        Log::warning("STATUS_CODE " . $info['http_code']);
-//        Log::warning("CURL " . $retorno);
+        $access_token = "EAAFdwO6fUOcBAJZCgCH2IHTuSezOyl2oK18Fyqs8LY5ZBEB8iZA7mtWBnl5xvSzeiWazemBXn1ZB4pqNZAyUVyHuUPu1oet4pk5ihehZBAZAZAAIVZAQFe4r2jlLngIzo062bCZAc2pMLvwmCc2a0hZCV24uwuEJbr2SZAIZC1CRXG18kelDmkpEN4k8H";
+        $curl = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=".$access_token);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($object));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json'
+        ));
+//        curl_exec($curl);
+        $retorno = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+
+        Log::info("======================================================================");
+        Log::warning("STATUS_CODE " . $info['http_code']);
+        Log::warning("CURL " . $retorno);
 
         return response(null, 200);
     }
