cf753e863f27307b41a89df6cbc10d856ece7bb1
Thiago <thiago@towty.com.br>
2018-09-02 10:06:19 -0300

    teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -36,17 +36,17 @@ class FacebookController extends Controller
         $object->recipient = $recipient;
         $object->message = $message;
 
-        $access_token = "EAAFdwO6fUOcBAJZCgCH2IHTuSezOyl2oK18Fyqs8LY5ZBEB8iZA7mtWBnl5xvSzeiWazemBXn1ZB4pqNZAyUVyHuUPu1oet4pk5ihehZBAZAZAAIVZAQFe4r2jlLngIzo062bCZAc2pMLvwmCc2a0hZCV24uwuEJbr2SZAIZC1CRXG18kelDmkpEN4k8H";
-        $curl = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=".$access_token);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($object));
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json'
-        ));
-//        curl_exec($curl);
-        $retorno = curl_exec($curl);
-        $info = curl_getinfo($curl);
-        curl_close($curl);
+//        $access_token = "EAAFdwO6fUOcBAJZCgCH2IHTuSezOyl2oK18Fyqs8LY5ZBEB8iZA7mtWBnl5xvSzeiWazemBXn1ZB4pqNZAyUVyHuUPu1oet4pk5ihehZBAZAZAAIVZAQFe4r2jlLngIzo062bCZAc2pMLvwmCc2a0hZCV24uwuEJbr2SZAIZC1CRXG18kelDmkpEN4k8H";
+//        $curl = curl_init("https://graph.facebook.com/v2.6/me/messages?access_token=".$access_token);
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($object));
+//        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+//                'Content-Type: application/json'
+//        ));
+////        curl_exec($curl);
+//        $retorno = curl_exec($curl);
+//        $info = curl_getinfo($curl);
+//        curl_close($curl);
 
         Log::info("======================================================================");
         Log::warning("STATUS_CODE " . $info['http_code']);
