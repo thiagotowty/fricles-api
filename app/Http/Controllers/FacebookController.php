2f2a18739fa69ad8434f9574d5ecd1b9558e5a9d
Thiago <thiago@towty.com.br>
2018-09-01 21:51:19 -0300

    teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -11,11 +11,11 @@ class FacebookController extends Controller
     {
 //        $verify_token = 'abcdefg12345678';
 //
-        $hub_challenge  = $request->hub_challenge;
+//        $hub_challenge  = $request->hub_challenge;
 //        $hub_verify_token  = $request->hub_verify_token;
 //
 //        if ($verify_token == $hub_verify_token)
-            return response($hub_challenge, 200);
+//            return response($hub_challenge, 200);
 //
 //        return response(null, 403);
 
