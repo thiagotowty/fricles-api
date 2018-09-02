81940cbd2ec6f16c8f0628e1389b9bb280481668
Thiago <thiago@towty.com.br>
2018-09-02 10:04:45 -0300

    teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -28,12 +28,12 @@ class FacebookController extends Controller
 
         $object = new class{};
         $object->messaging_type = "RESPONSE";
-        $receipt = new class{};
-        $receipt->id = $request->entry[0]["messaging"][0]["sender"]["id"];
+        $recipient = new class{};
+        $recipient->id = $request->entry[0]["messaging"][0]["sender"]["id"];
         $message = new class{};
         $message->text = "Teste legal";
 
-        $object->receipt = $receipt;
+        $object->recipient = $recipient;
         $object->message = $message;
 
         $access_token = "EAAFdwO6fUOcBAJZCgCH2IHTuSezOyl2oK18Fyqs8LY5ZBEB8iZA7mtWBnl5xvSzeiWazemBXn1ZB4pqNZAyUVyHuUPu1oet4pk5ihehZBAZAZAAIVZAQFe4r2jlLngIzo062bCZAc2pMLvwmCc2a0hZCV24uwuEJbr2SZAIZC1CRXG18kelDmkpEN4k8H";
